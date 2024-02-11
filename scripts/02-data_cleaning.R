d6bd412eabf95f765352563369b5e5facd91f9a1
Aryaman Sharma <aryaman.sharma@mail.utoronto.ca>
2024-02-11 00:32:46 -0500

    Added all the code for 99-replications.R and 02-data_cleaning.R. This is code for cleaning the data from the package of the paper referred to and for replicating the figures in the paper. 02-data_cleaning.R generated clean files which have been saved to data/analysis_data.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -8,37 +8,201 @@
 # Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
+library(ggplot2)
 library(tidyverse)
+library(janitor)
+library(readxl)
+library(dplyr)
+library(rnaturalearth)
+library(sf)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# File 1: GHS_index.xlsx
+ghs_index_data <- read_excel("data/raw_data/GHS_index.xlsx")
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+cleaned_ghs_index_data <- ghs_index_data |>
+  clean_names()|>
+  select(
+    `country`,
+    `year`,
+    `overall_score`
+  )
+
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Russian Federation"] <- "Russia"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Bosnia and Herzegovina"] <- "Bosnia"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Bolivia (Plurinational State of)"] <- "Bolivia"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Venezuela (Bolivarian Republic of)"] <- "Venezuela"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Iran (Islamic Republic of)"] <- "Iran"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "CÃ´te d'Ivoire"] <- "Côte d'Ivoire"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Democratic Republic of the Congo"] <- "Dem. Rep. Congo"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "South Sudan"] <- "S. Sudan"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "United Republic of Tanzania"] <- "Tanzania"
+cleaned_ghs_index_data$country[cleaned_ghs_index_data$country == "Central African Republic"] <- "Central African Rep."
+
+cleaned_ghs_index_data_2021 <- cleaned_ghs_index_data |>
+  filter(year == 2021) |>
+  select(
+    `country`,
+    `overall_score`
+  )
+write_csv(cleaned_ghs_index_data_2021, "data/analysis_data/cleaned_GHS_index.csv")
+
+# File 2: life_table.csv
+
+life_expectancy_data <- read_csv("data/raw_data/life_table.csv")
+
+cleaned_life_expectancy_data <- life_expectancy_data |>
+  clean_names() |>
+  select(
+    year_id,
+    all,
+    hispanic,
+    non_hispanic_black,
+    non_hispanic_white,
+  ) |>
+  rename(
+    Year = year_id,
+    `All Races` = all,
+    Hispanic = hispanic,
+    Black = non_hispanic_black,
+    White = non_hispanic_white
+  )
+write_csv(cleaned_life_expectancy_data, "data/analysis_data/cleaned_life_table.csv")
+
+cleaned_life_expectancy_data_all_races <- life_expectancy_data |>
+  clean_names() |>
+  select(
+    year_id,
+    all,
+    hispanic,
+    non_hispanic_aian,
+    non_hispanic_asian,
+    non_hispanic_black,
+    non_hispanic_white,
+  ) |>
+  rename(
+    Year = year_id,
+    `All Races` = all,
+    Hispanic = hispanic,
+    AIAN = non_hispanic_aian,
+    Asian = non_hispanic_asian,
+    Black = non_hispanic_black,
+    White = non_hispanic_white
+  )
+write_csv(cleaned_life_expectancy_data_all_races, "data/analysis_data/cleaned_life_table_all_races.csv")
+
+# File 3: all_cause_dth_rates.csv
+
+deaths_data <- read_csv("data/raw_data/all_cause_dth_rates.csv")
+
+cleaned_deaths_data_rep <- deaths_data |>
+  clean_names() |>
+  select(
+    country_name,
+    `x2013`,
+    `x2014`,
+    `x2015`,
+    `x2016`,
+    `x2017`,
+    `x2018`,
+    `x2019`,
+    `x2020`,
+    `x2021`
+  ) |>
+  filter(country_name %in% c("Germany", "United Kingdom", "United States", "New Zealand", "Australia", "Korea, Rep.")) |>
+  rename(
+    `2013` = `x2013`,
+    `2014` = `x2014`,
+    `2015` = `x2015`,
+    `2016` = `x2016`,
+    `2017` = `x2017`,
+    `2018` = `x2018`,
+    `2019` = `x2019`,
+    `2020` = `x2020`,
+    `2021` = `x2021`
+  )
+write_csv(cleaned_deaths_data_rep, "data/analysis_data/cleaned_all_cause_dth_rates_rep.csv")
+
+cleaned_deaths_data <- deaths_data |>
+  clean_names() |>
+  select(
+    country_name,
+    `x2010`,
+    `x2011`,
+    `x2012`,
+    `x2013`,
+    `x2014`,
+    `x2015`,
+    `x2016`,
+    `x2017`,
+    `x2018`,
+    `x2019`,
+    `x2020`,
+    `x2021`
+  ) |>
+  filter(country_name %in% c("India", "United States", "Russian Federation", "Korea, Rep.")) |>
+  rename(
+    `2010` = `x2010`,
+    `2011` = `x2011`,
+    `2012` = `x2012`,
+    `2013` = `x2013`,
+    `2014` = `x2014`,
+    `2015` = `x2015`,
+    `2016` = `x2016`,
+    `2017` = `x2017`,
+    `2018` = `x2018`,
+    `2019` = `x2019`,
+    `2020` = `x2020`,
+    `2021` = `x2021`
+  )
+
+write_csv(cleaned_deaths_data, "data/analysis_data/cleaned_all_cause_dth_rates.csv")
+
+# File 4: IHME_excess_deaths.csv and GHS_index.xlsx combined
+
+ihme_excess_deaths_data <- read_csv("data/raw_data/IHME_excess_deaths.csv")
+
+cleaned_ihme_excess_deaths_data <- ihme_excess_deaths_data |>
+  clean_names() |>
+  select(
+    location_name,
+    measure_name,
+    mean_value
+  ) |>
+  filter(measure_name == "excess_death_rate") |>
+  select(
+    location_name,
+    mean_value
+  ) |>
+  rename(
+    country = location_name,
+    excess_death_rate = mean_value
+  ) |>
+  filter(country %in% c("India", "USA", "Russia", "UK", "Australia", "Canada", "Netherlands", "Spain", "France", "Japan", "Denmark", "Germany", "Austria", "Brazil", "Hungary", "Indonesia"))
+
+cleaned_ghs <- ghs_index_data |>
+  clean_names() |>
+  select(
+    `country`,
+    `year`,
+    `overall_score`
+  ) |>
+  filter(year == 2021) |>
+  rename(
+    ghs_index = overall_score
+  ) |>
+  filter(country %in% c("India", "United States of America", "Russian Federation", "United Kingdom", "South Korea", "Australia", "Canada", "Netherlands", "Spain", "France", "Japan", "Denmark", "Germany", "Austria", "Brazil", "Hungary", "Indonesia")) |>
+  select(
+    `country`,
+    `ghs_index`
+  )
+
+cleaned_ghs$country[cleaned_ghs$country == "Russian Federation"] <- "Russia"
+cleaned_ghs$country[cleaned_ghs$country == "United States of America"] <- "USA"
+cleaned_ghs$country[cleaned_ghs$country == "United Kingdom"] <- "UK"
+
+combined_data <- merge(cleaned_ihme_excess_deaths_data, cleaned_ghs, by = "country", all = TRUE)
+
+write_csv(combined_data, "data/analysis_data/cleaned_excess_death_ghs.csv")
+
--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,19 +1,195 @@
 #### Preamble ####
 # Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Aryaman Sharma
+# Date: 10 February 2024
+# Contact: aryaman.sharma@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
+library(ggplot2)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(dplyr)
+library(rnaturalearth)
+library(sf)
 
 #### Load data ####
-# [...ADD CODE HERE...]
+
+# Figure 1
+cleaned_ghs_index_data_2021 <- read_csv("data/analysis_data/cleaned_GHS_index.csv")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+world_scores <- merge(world, cleaned_ghs_index_data_2021, by.x = "name", by.y = "country", all.x = TRUE)
+
+world_scores$score_factor <- cut(
+  world_scores$overall_score, 
+  breaks = c(16, 25, 30, 35, 40, 45, 50, 60, 70, 76), 
+  labels = c("16 to 25", "25 to 30", "30 to 35", "35 to 40", "40 to 45", "45 to 50", "50 to 60", "60 to 70", "70 to 76"), 
+  include.lowest = TRUE)
+world_scores
+
+ggplot(data = world_scores) +
+  geom_sf(aes(fill = score_factor), color = "white") +
+  scale_fill_manual(values = c("#990000", "#CC3333", "#FF6666", "#FF9966", "#FFCC99", "#99CCFF", "#6699CC", "#336699", "#003399")) +
+  labs(fill = "Global health security score", title = "Global Health Security Score (2021)") +
+  theme_minimal()
+
+# Figure 2 (Part A)
+cleaned_life_expectancy_data <- read_csv("data/analysis_data/cleaned_life_table.csv")
+
+cleaned_life_expectancy_data_long <- pivot_longer(cleaned_life_expectancy_data, cols = -Year, names_to = "Race", values_to = "Value")
+
+ggplot(cleaned_life_expectancy_data_long, aes(x = Year, y = Value, color = Race)) +
+  geom_line() +
+  geom_point() +
+  theme_classic() +
+  scale_y_continuous(breaks = 65:85) +
+  scale_x_continuous(breaks = 2006:2021) +
+  labs(title = "Life Expectancy by Race", x = "Year", y = "Life Expectancy") +
+  scale_color_brewer(palette = "Set1")
+
+# Figure 2 (Part B)
+cleaned_life_expectancy_data_all_races <- read_csv("data/analysis_data/cleaned_life_table_all_races.csv")
+
+cleaned_life_expectancy_data_all_races_short <- cleaned_life_expectancy_data_all_races |>
+  filter(Year >= 2019)
+
+change_in_life_expectancy <- cleaned_life_expectancy_data_all_races_short |>
+  summarise(
+    Period = c("2019-2020", "2020-2021"),
+    `All Races` = c(`All Races`[2] - `All Races`[1], `All Races`[3] - `All Races`[2]),
+    Hispanic = c(Hispanic[2] - Hispanic[1], Hispanic[3] - Hispanic[2]),
+    `American Indians and Alaska Native` = c(AIAN[2] - AIAN[1], AIAN[3] - AIAN[2]),
+    Asian = c(Asian[2] - Asian[1], Asian[3] - Asian[2]),
+    Black = c(Black[2] - Black[1], Black[3] - Black[2]),
+    White = c(White[2] - White[1], White[3] - White[2])
+  )
+
+cleaned_life_expectancy_data_all_races_long <- pivot_longer(change_in_life_expectancy, cols = -Period, names_to = "Category", values_to = "Difference")
+
+ggplot(cleaned_life_expectancy_data_all_races_long, aes(x = Category, y = Difference, fill = Period)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+  theme_minimal() +
+  labs(title = "Yearly Differences in Life Expectancy by Category",
+       x = "Period",
+       y = "Difference in Life Expectancy") +
+  scale_fill_brewer(palette = "Set2") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_text(aes(label = round(Difference, 1)), position = position_dodge(width = 0.9), vjust = 1.2, color = "black")
+
+# Figure 3 (Part A)
+cleaned_deaths_data_rep <- read_csv("data/analysis_data/cleaned_all_cause_dth_rates_rep.csv")
+
+cleaned_deaths_data_long <- pivot_longer(cleaned_deaths_data_rep, cols = -country_name, names_to = "Year", values_to = "Value")
+
+cleaned_deaths_data_long[,-1] <- lapply(cleaned_deaths_data_long[,-1], as.numeric)
+
+cleaned_deaths_data_fit <- cleaned_deaths_data_long |>
+  filter(Year >= 2013 & Year <= 2019)
+
+ggplot(cleaned_deaths_data_long, aes(x = Year, y = Value, group = country_name, color = country_name)) +
+  geom_line(aes(color = country_name)) +
+  geom_point(aes(color = country_name)) +
+  geom_smooth(data = cleaned_deaths_data_fit, method = "lm", se = FALSE, aes(color = country_name), linetype = "dashed", size = 0.5, alpha = 0.1) +
+  theme_minimal() +
+  scale_x_continuous(breaks = 2013:2021) +
+  labs(title = "Values Over Years by Country", x = "Year", y = "Value") +
+  theme(legend.title = element_blank())
+
+# Figure 3 (Part B)
+
+# Model: Australia
+model_australia <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "Australia"))
+# Predicting 2020 and 2021
+coefficients_aus <- coef(model_australia)
+aus_2020 <- (coefficients_aus["Year"] * 2020) + coefficients_aus["(Intercept)"]
+aus_2021 <- (coefficients_aus["Year"] * 2021) + coefficients_aus["(Intercept)"]
+# Ratio
+aus <- cleaned_deaths_data_long |>
+  filter(country_name == "Australia")
+excess_aus_2020 <- aus$Value[8] - aus_2020
+excess_aus_2021 <- aus$Value[9] - aus_2021
+
+# Model: Germany
+model_germany <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "Germany"))
+# Predicting 2020 and 2021
+coefficients_ger <- coef(model_germany)
+ger_2020 <- (coefficients_ger["Year"] * 2020) + coefficients_ger["(Intercept)"]
+ger_2021 <- (coefficients_ger["Year"] * 2021) + coefficients_ger["(Intercept)"]
+# Ratio
+ger <- cleaned_deaths_data_long |>
+  filter(country_name == "Germany")
+excess_ger_2020 <- ger$Value[8] - ger_2020
+excess_ger_2021 <- ger$Value[9] - ger_2021
+
+# Model: United Kingdom
+model_uk <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "United Kingdom"))
+# Predicting 2020 and 2021
+coefficients_uk <- coef(model_uk)
+uk_2020 <- (coefficients_uk["Year"] * 2020) + coefficients_uk["(Intercept)"]
+uk_2021 <- (coefficients_uk["Year"] * 2021) + coefficients_uk["(Intercept)"]
+# Ratio
+uk <- cleaned_deaths_data_long |>
+  filter(country_name == "United Kingdom")
+excess_uk_2020 <- uk$Value[8] - uk_2020
+excess_uk_2021 <- uk$Value[9] - uk_2021
+
+# Model: United States
+model_us <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "United States"))
+# Predicting 2020 and 2021
+coefficients_us <- coef(model_us)
+us_2020 <- (coefficients_us["Year"] * 2020) + coefficients_us["(Intercept)"]
+us_2021 <- (coefficients_us["Year"] * 2021) + coefficients_us["(Intercept)"]
+# Ratio
+us <- cleaned_deaths_data_long |>
+  filter(country_name == "United States")
+excess_us_2020 <- us$Value[8] - us_2020
+excess_us_2021 <- us$Value[9] - us_2021
+
+# Model: New Zealand
+model_nz <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "New Zealand"))
+# Predicting 2020 and 2021
+coefficients_nz <- coef(model_nz)
+nz_2020 <- (coefficients_nz["Year"] * 2020) + coefficients_nz["(Intercept)"]
+nz_2021 <- (coefficients_nz["Year"] * 2021) + coefficients_nz["(Intercept)"]
+# Ratio
+nz <- cleaned_deaths_data_long |>
+  filter(country_name == "New Zealand")
+excess_nz_2020 <- nz$Value[8] - nz_2020
+excess_nz_2021 <- nz$Value[9] - nz_2021
+
+# Model: South Korea
+model_sk <- lm(Value ~ Year, data = cleaned_deaths_data_fit |> filter(country_name == "Korea, Rep."))
+# Predicting 2020 and 2021
+coefficients_sk <- coef(model_sk)
+sk_2020 <- (coefficients_sk["Year"] * 2020) + coefficients_sk["(Intercept)"]
+sk_2021 <- (coefficients_sk["Year"] * 2021) + coefficients_sk["(Intercept)"]
+# Ratio
+sk <- cleaned_deaths_data_long |>
+  filter(country_name == "Korea, Rep.")
+excess_sk_2020 <- sk$Value[8] - sk_2020
+excess_sk_2021 <- sk$Value[9] - sk_2021
+
+excess_deaths <- tibble(
+  Year = c(2020, 2020, 2020, 2020, 2020, 2020, 2021, 2021, 2021, 2021, 2021, 2021),
+  country_name = rep(c("Australia", "Germany", "United Kingdom", "United States", "New Zealand", "Korea, Rep."), times = 2),
+  Value = c(excess_aus_2020, excess_ger_2020, excess_uk_2020, excess_us_2020, excess_nz_2020, excess_sk_2020, excess_aus_2021, excess_ger_2021, excess_uk_2021, excess_us_2021, excess_nz_2021, excess_sk_2021) |> round(2)
+)
+
+ggplot(excess_deaths, aes(x = country_name, y = Value, fill = as.factor(Year))) +
+  geom_bar(stat = "identity", position = position_dodge(), width = 0.7) +
+  coord_flip() +  # Flip coordinates to make the bar plot horizontal
+  theme_minimal() +
+  labs(title = "Value by Country and Year", x = "Country", y = "Value") +
+  scale_fill_manual(values = c("2021" = "blue", "2020" = "lightblue"), name = "Year") +
+  geom_text(aes(label = Value), position = position_dodge(width = 0.9), color = "black")
+
+
+
 
 
 
